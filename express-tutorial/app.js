8d1d47f685531eafdf37bc89d90acf6321910424
varunpats <varunpatil2502@gmail.com>
2024-05-04 23:59:15 +0530

    Sending data to URL

--- express-tutorial/app.js
+++ express-tutorial/app.js
@@ -1,9 +1,19 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    res.write('<h1>Hello world</h1>')
-    res.end()
+    if (req.url === '/') {
+        res.writeHead(200, { 'content-type': 'text/html' })
+        res.write('<h1>Hello world</h1>')
+        res.end()
+    } else if (req.url === '/about') {
+        res.writeHead(200, { 'content-type': 'text/html' })
+        res.write('<h1>About page</h1>')
+        res.end()
+    } else {
+        res.writeHead(404, { 'content-type': 'text/html' })
+        res.write('<h1>404</h1>')
+        res.end()
+    }
 })
 
 server.listen(5000)

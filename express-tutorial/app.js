768a197decc0db1db926439990cf3f0f636a2026
varunpats <varunpatil2502@gmail.com>
2024-05-15 22:39:14 +0530

    Use of app.use

--- express-tutorial/app.js
+++ express-tutorial/app.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const app = express()
+const logger = require('./logger');
 
-const logger = (req, res, next) => {
-    const method = req.method;
-    const url = req.url;
-    const time = new Date().getFullYear()
-    console.log(method, url, time);
-    next()
-}
+app.use('/api', logger)
 
-app.get('/', logger, (req, res) => {
+app.get('/', (req, res) => {
     res.send('Home')
 })
 
-app.get('/about', logger, (req, res) => {
+app.get('/about', (req, res) => {
     res.send('About')
+})
+
+app.get('/api/products', (req, res) => {
+    res.send('Products')
+})
+
+app.get('/api/items', (req, res) => {
+    res.send('Items')
 })
 
 app.listen(5000, () => {

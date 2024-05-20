242a00d0438595f75716998bfe4cfa578626ecf7
varunpats <varunpatil2502@gmail.com>
2024-05-20 17:04:45 +0530

    Handling post request

--- express-tutorial/app.js
+++ express-tutorial/app.js
@@ -1,25 +1,33 @@
 const express = require('express');
-const app = express()
-const logger = require('./logger');
-const authorize = require('./authorize');
+const app = express();
+const { people } = require('./data')
 
-app.use([logger, authorize])
+app.use(express.static('./methods-public'))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Home')
 })
 
-app.get('/about', (req, res) => {
-    res.send('About')
-    console.log(req.user);
+app.get('/api/people', (req, res) => {
+    res.status(200).json({ success: true, data: people })
 })
 
-app.get('/api/products', (req, res) => {
-    res.send('Products')
+app.post('/api/people', (req, res) => {
+    const { name } = req.body;
+    if (!name) {
+        return res.status(400).json({success: false, msg: 'Please provide the name'})
+    }
+    res.status(201).json({success: true, person: name})
 })
 
-app.get('/api/items', (req, res) => {
-    res.send('Items')
+app.post('/login', (req, res) => {
+    const { name } = req.body;
+    if (name) {
+        return res.status(200).send(`Welcome ${name}`)
+    }
+    res.status(401).send('Please provide the value')
 })
 
 app.listen(5000, () => {

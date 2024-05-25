d7c3536e82cd1227df2a8e697a495b3b16a09adc
varunpats <varunpatil2502@gmail.com>
2024-05-25 19:12:39 +0530

    Handled PUT and DELETE requests

--- express-tutorial/app.js
+++ express-tutorial/app.js
@@ -17,9 +17,41 @@ app.get('/api/people', (req, res) => {
 app.post('/api/people', (req, res) => {
     const { name } = req.body;
     if (!name) {
-        return res.status(400).json({success: false, msg: 'Please provide the name'})
+        return res.status(400).json({ success: false, msg: 'Please provide the name' })
     }
-    res.status(201).json({success: true, person: name})
+    res.status(201).json({ success: true, person: name })
+})
+
+app.put('/api/people/:id', (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+
+    const person = people.find(person => person.id === +id)
+
+    if (!person) {
+        res.status(404).send(`Can't find person with id ${id}`)
+    }
+
+    const newPeople = people.map(person => {
+        if (person.id === +id) {
+            person.name = name
+        }
+        return person
+    })
+
+    res.status(200).send(newPeople)
+})
+
+app.delete('/api/people/:id', (req, res) => {
+    const person = people.find(person => person.id === +req.params.id)
+
+    if (!person) {
+        res.status(404).send(`Can't find person with id ${req.params.id}`)
+    }
+
+    const newPeople = people.filter(person => person.id !== +req.params.id)
+
+    res.status(200).send(newPeople)
 })
 
 app.post('/login', (req, res) => {
